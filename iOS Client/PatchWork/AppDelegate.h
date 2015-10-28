5a8532af799ebb6582c1830274536a2ef5fec7db
Kiboom <cheyugy@naver.com>
2015-10-28 14:51:28 +0900

    첫번째

---
+++ iOS Client/PatchWork/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PatchWork
+//
+//  Created by 김기범 on 2015. 10. 21..
+//  Copyright © 2015년 NEXT Institute. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOS Client/PatchWork/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PatchWork
+//
+//  Created by 김기범 on 2015. 10. 21..
+//  Copyright © 2015년 NEXT Institute. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

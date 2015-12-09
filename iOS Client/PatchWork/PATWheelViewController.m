2f393cd05abac069dc0e9b124b2179889ef98d5f
Kiboom <cheyugy@naver.com>
2015-12-09 10:36:23 +0900

    휠 ;

--- iOS Client/PatchWork/PATWheelViewController.m
+++ iOS Client/PatchWork/PATWheelViewController.m
@@ -117,36 +117,43 @@ float bearing = 0.0;
         self.position.text = @"JOY";
         self.position.textColor = [UIColor colorWithRed:(199/255.f) green:(154/255.f) blue:(23/255.f)
                                                   alpha:1.0];
+        [self.emotionInWheel setImage:[UIImage imageNamed:@"joy"]];
 }
     else if (bearing>=45 && bearing<90){
         self.position.text = @"TIRED";
         self.position.textColor = [UIColor colorWithRed:(114/255.f) green:(80/255.f) blue:(46/255.f)
                                                   alpha:1.0];
-        //[self.emotionInWheel.image
+         [self.emotionInWheel setImage:[UIImage imageNamed:@"tired"]];
     }
     else if (bearing>=90 && bearing<135){
         self.position.text = @"FUN";
         self.position.textColor = [UIColor colorWithRed:(199/255.f) green:(87/255.f) blue:(25/255.f)alpha:1.0];
+        [self.emotionInWheel setImage:[UIImage imageNamed:@"fun"]];
     }
     else if (bearing>=135 && bearing<180){
         self.position.text = @"ANGRY";
         self.position.textColor = [UIColor colorWithRed:(194/255.f) green:(46/255.f) blue:(66/255.f)alpha:1.0];
+        [self.emotionInWheel setImage:[UIImage imageNamed:@"angry"]];
     }
     else if (bearing>=180 && bearing<225){
         self.position.text = @"SURPRISED";
         self.position.textColor = [UIColor colorWithRed:(208/255.f) green:(94/255.f) blue:(142/255.f)alpha:1.0];
+        [self.emotionInWheel setImage:[UIImage imageNamed:@"surprised"]];
     }
     else if (bearing>=225 && bearing<270){
         self.position.text = @"SCARED";
         self.position.textColor = [UIColor colorWithRed:(117/255.f) green:(62/255.f) blue:(146/255.f)alpha:1.0];
+        [self.emotionInWheel setImage:[UIImage imageNamed:@"scared"]];
     }
     else if (bearing>=270 && bearing<315){
         self.position.text = @"SAD";
         self.position.textColor = [UIColor colorWithRed:(79/255.f) green:(111/255.f) blue:(217/255.f)alpha:1.0];
+        [self.emotionInWheel setImage:[UIImage imageNamed:@"sad"]];
     }
     else{
         self.position.text = @"EXCITED";
         self.position.textColor = [UIColor colorWithRed:(90/255.f) green:(212/255.f) blue:(194/255.f)alpha:1.0];
+        [self.emotionInWheel setImage:[UIImage imageNamed:@"excited"]];
     }
 }
 

7abe9a1a2862acf842235234393a5a9adae635b1
Kiboom <cheyugy@naver.com>
2015-12-29 04:15:39 +0900

    localhost로 원상복구

--- iOS Client/PatchWork/PATWheelViewController.m
+++ iOS Client/PatchWork/PATWheelViewController.m
@@ -425,7 +425,7 @@
 	NSString *postLength = [NSString stringWithFormat:@"%lu", (unsigned long)[postData length]];
 	
 	NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init];
-	[request setURL:[NSURL URLWithString:@"http://192.168.1.135:5000/insertEmotion"]];
+	[request setURL:[NSURL URLWithString:@"http://localhost:5000/insertEmotion"]];
 	[request setHTTPMethod:@"POST"];
 	[request setValue:postLength forHTTPHeaderField:@"Content-Length"];
 	[request setValue:@"application/x-www-form-urlencoded" forHTTPHeaderField:@"Content-Type"];
